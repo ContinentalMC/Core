53b32dbb1b500bb62af0f65ff3ed71ff72a8e803
Nikki <136409902+Nikkilectric@users.noreply.github.com>
2024-06-11 11:01:55 -0600

    Level 4

--- src/main/java/org/cmc/levelperms/LevelPermissions.java
+++ src/main/java/org/cmc/levelperms/LevelPermissions.java
@@ -1,36 +1,34 @@
-// package org.cmc.levelperms;
+ package org.cmc.levelperms;
 
-// import org.bukkit.entity.Player;
-// import org.bukkit.event.EventHandler;
-// import org.bukkit.event.EventPriority;
-// import org.bukkit.event.Listener;
-// import org.bukkit.event.player.PlayerMoveEvent;
+ import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+ import org.bukkit.event.EventHandler;
+ import org.bukkit.event.EventPriority;
+ import org.bukkit.event.Listener;
+ import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.permissions.Permission;
+import org.jetbrains.annotations.NotNull;
 
-// import org.jetbrains.annotations.NotNull;
 
+ import me.clip.placeholderapi.PlaceholderAPI;
 
-// import me.clip.placeholderapi.PlaceholderAPI;
-// import net.luckperms.api.model.data.DataMutateResult;
-
-// public class LevelPermissions implements @NotNull Listener {
+ public class LevelPermissions implements @NotNull Listener {
     
-//         @EventHandler(priority = EventPriority.HIGHEST)
-    // Check every tick for player levels
-//    public void tick(PlayerMoveEvent event) {
- //       Player player = event.getPlayer();
- //       String result = PlaceholderAPI.setPlaceholders(player, "%auraskills_average_int%");
- //       try {
-  //          int level = Integer.parseInt(result);
-  //          if ((level > 15)&&(level < 18)) {
-   //         // Execute actions based on the result
-   //         player.chat("I'm level!"+ level);
-   //         // Give the player a permission
-   //         DataMutateResult result = user.data().add(Node.builder("your.node.here").build());
-//
-   //     }
-  //  }   finally {}
-  //  }
-// }
-
-    
+         @EventHandler(priority = EventPriority.HIGHEST)
+     // Check every tick for player levels
+    public void tick(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        String result = PlaceholderAPI.setPlaceholders(player, "%auraskills_average_int%");
+        try {
+          // Level 4
+            int level = Integer.parseInt(result);
+            if ((level >= 4)) {
+            // Execute actions based on the result
+            player.chat("I'm level!"+ level);
+            // Give the player a permission
+            Bukkit.getServer().getPluginManager().addPermission(new Permission("cmc.level.4"));
+        }
+    }   finally {}
+    }
+ }
 
