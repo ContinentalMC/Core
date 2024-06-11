5c5de042d0e72a5325c5edbbd598881e35fba796
Nikki <136409902+Nikkilectric@users.noreply.github.com>
2024-06-11 10:38:12 -0600

    remove onenable/ondisable

--- src/main/java/org/cmc/dropitems/DropItems.java
+++ src/main/java/org/cmc/dropitems/DropItems.java
@@ -1,17 +1,7 @@
 package org.cmc.dropitems;
 
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class DropItems extends JavaPlugin {
 
-    
-    public void onEnable() {
-
-    }
-
-    
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
 }
