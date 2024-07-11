ea540b31d79bed55b20a7fed6bb5894c86de6d1f
Nikki <136409902+Nikkilectric@users.noreply.github.com>
2024-07-10 23:46:11 -0600

    Remove levelperms

--- src/main/java/org/cmc/CMC.java
+++ src/main/java/org/cmc/CMC.java
@@ -3,7 +3,6 @@ package org.cmc;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.cmc.dropitems.*;
-import org.cmc.levelperms.LevelPermissions;
 import org.cmc.movecraft.RotateEvent;
 
 public final class CMC extends JavaPlugin {
@@ -18,7 +17,6 @@ public final class CMC extends JavaPlugin {
         ConfigManager.setupConfig(this);
         CMCutil.StartTimer();
         Bukkit.getPluginManager().registerEvents(new TeleportDetectListener(), this);
-        Bukkit.getPluginManager().registerEvents(new LevelPermissions(), this);
         Bukkit.getPluginManager().registerEvents(new RotateEvent(), this);
     }
 
