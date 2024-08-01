3a3772fa23f868e19d24235b944b24876e4957c4
Nikki <136409902+Nikkilectric@users.noreply.github.com>
2024-07-31 22:34:03 -0600

    Remove broken / Unusded Imports

--- src/main/java/org/cmc/shops/shopcommand.java
+++ src/main/java/org/cmc/shops/shopcommand.java
@@ -1,22 +1,16 @@
 package org.cmc.shops;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-import com.ghostchu.quickshop.QuickShop;
-
 public class shopcommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
-            QuickShop api = QuickShop.getInstance();
             // Let's insert our code here when we can figure out the API
             return true;
         } else {
