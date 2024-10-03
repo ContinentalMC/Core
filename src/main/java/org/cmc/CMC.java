7eb0138cadd7c105f058c01b34fd7bed3a093470
Nikki <136409902+Nikkilectric@users.noreply.github.com>
2024-10-02 19:47:19 -0600

    Remove ShopCommand

--- src/main/java/org/cmc/CMC.java
+++ src/main/java/org/cmc/CMC.java
@@ -4,7 +4,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.cmc.dropitems.*;
 import org.cmc.movecraft.RotateEvent;
-import org.cmc.shops.shopcommand;
 
 public final class CMC extends JavaPlugin {
 
@@ -21,8 +20,6 @@ public final class CMC extends JavaPlugin {
         Bukkit.getPluginManager().registerEvents(new TeleportDetectListener(), this);
         Bukkit.getPluginManager().registerEvents(new DeathListener(), this);
         Bukkit.getPluginManager().registerEvents(new RotateEvent(), this);
-        // Commands
-        this.getCommand("shops").setExecutor(new shopcommand());
     }
 
     @Override
