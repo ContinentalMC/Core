fb6328b77190c11f62e25a438c43103cacd576d7
Nikki <136409902+Nikkilectric@users.noreply.github.com>
2024-10-24 22:34:36 -0600

    I hate enchantments so much

--- src/main/java/org/cmc/CMC.java
+++ src/main/java/org/cmc/CMC.java
@@ -6,8 +6,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.cmc.dropitems.*;
 import org.cmc.items.*;
 import org.cmc.movecraft.RotateEvent;
-import org.cmc.items.EnchantItemListener;
-import org.cmc.items.InventoryClickListener;
 
 
 public final class CMC extends JavaPlugin {
@@ -25,19 +23,10 @@ public final class CMC extends JavaPlugin {
         Bukkit.getPluginManager().registerEvents(new TeleportDetectListener(), this);
         Bukkit.getPluginManager().registerEvents(new DeathListener(), this);
         Bukkit.getPluginManager().registerEvents(new RotateEvent(), this);
-        Bukkit.getPluginManager().registerEvents(new PickupItemListener(), this);
-        Bukkit.getPluginManager().registerEvents(new EnchantmentPreviewListener(), this);
-        Bukkit.getPluginManager().registerEvents(new InventoryClickListener(), this);
-        Bukkit.getPluginManager().registerEvents(new EnchantItemListener(), this);
-        // Commands
-        this.getCommand("getenchantments").setExecutor(new EnchantmentsCommand());
 
         // Integrations
-        if (getServer().getPluginManager().getPlugin("PlaceholderAPI") != null) {
-            new EnchantmentsPlaceholder(this).register();
-        }
 
-
+        // Startup
         CMCutil.log("   .----------------. .----------------. .----------------. ", null);
         CMCutil.log("  | .--------------. | .--------------. | .--------------. |", null);
         CMCutil.log("  | |     ______   | | | ____    ____ | | |     ______   | |", null);
--- src/main/java/org/cmc/CMCutil.java
+++ src/main/java/org/cmc/CMCutil.java
@@ -2,11 +2,8 @@ package org.cmc;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.ConsoleCommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
-import org.cmc.items.LoreUpdater;
 
 import net.md_5.bungee.api.ChatColor;
 
@@ -25,24 +22,7 @@ public class CMCutil {
                 Bukkit.getServer().getPluginManager().callEvent(stu);
             }
         }.runTaskTimer(CMCutil.getPlugin(), 20L, 20L); // Every second
-
-        // Timer for lore updates
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                updateLoreForAllPlayers();
-            }
-        }.runTaskTimer(CMCutil.getPlugin(), 0L, 40L); // Every 2 seconds (40 ticks)
     }
-
-    // Method to update lore for all players
-    private static void updateLoreForAllPlayers() {
-        for (Player player : Bukkit.getOnlinePlayers()) {
-            ItemStack item = player.getInventory().getItemInMainHand();
-            LoreUpdater.updateItemLoreWithEnchantments(item); // Update the lore with enchantments
-        }
-    }
-
     // Debug messages in console
     public static void debug(String message) {
         ConsoleCommandSender sender = Bukkit.getConsoleSender();
