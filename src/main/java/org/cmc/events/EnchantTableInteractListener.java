8e2469e5a4e35f064b26c5a0385a17e6ac2024e7
Nikki <136409902+Nikkilectric@users.noreply.github.com>
2024-10-25 22:50:38 -0600

    Run cancel event AFTER sending the command + fix

--- src/main/java/org/cmc/events/EnchantTableInteractListener.java
+++ src/main/java/org/cmc/events/EnchantTableInteractListener.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class EnchantTableInteractListener implements Listener {
@@ -11,15 +12,13 @@ public class EnchantTableInteractListener implements Listener {
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
         // Check if the player interacted with an enchantment table
-        if (event.getClickedBlock() != null && event.getClickedBlock().getType() == Material.ENCHANTING_TABLE) {
-            // Cancel the event
-            event.setCancelled(true);
-
+        if (event.getAction() == Action.RIGHT_CLICK_BLOCK && event.getClickedBlock() != null && event.getClickedBlock().getType() == Material.ENCHANTING_TABLE)  {
             // Get the player's name
             String playerName = event.getPlayer().getName();
 
             // Run the command in the console
             Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "mi stations open imbuement-table " + playerName);
+            event.setCancelled(true);
         }
     }
 }
